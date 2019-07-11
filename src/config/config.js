5039ea40f3bb0f41d6533ba64f4e5681cc8c5805
Bounteous <bounteous17@protonmail.com>
2019-07-11 16:43:58 +0200

    Update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,16 @@
 module.exports = {
+  env: {
+    processVersion: '10.16',
+  },
   webServer: {
-    origin: "0.0.0.0",
-    port: 2424
-  }
+    origin: '0.0.0.0',
+    port: 2424,
+  },
+  mongo: {
+    uri: 'mongodb://localhost:27017/pecron',
+  },
+  server: {
+    adminRandomPasswordStorageFile: '.ADMIN_PASSWORD',
+    adminEmail: 'admin@pecron.gpl'
+  },
 };
