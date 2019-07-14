5cd2921c3270dc9eb27bab0a37d6e4f24e4e461e
Bounteous <bounteous17@protonmail.com>
2019-07-14 13:43:16 +0200

    Replace jwt-redis

--- src/modules/redis.module.js
+++ src/modules/redis.module.js
@@ -1,27 +1,29 @@
 const debug = require('debug')('src/modules/mem.db.module');
 const _Config = require('../config/config');
 const _RedisJwt = require('redis-jwt');
-const redisJwt = new _RedisJwt({
-  //host: '/tmp/redis.sock', //unix domain
-  host: _Config.redis.host, //can be IP or hostname
-  port: _Config.redis.port, // port
-  maxretries: _Config.redis.maxretries, //reconnect retries, default 10
-  //auth: '123', //optional password, if needed
-  db: _Config.jwt.redisDb, //optional db selection
-  secret: _Config.jwt.secret, // secret key for Tokens!
-  multiple: _Config.jwt.multiple, // single or multiple sessions by user
-  kea: _Config.redis.kea, // Enable notify-keyspace-events KEA
-});
+const __instances = {
+  jwt: new _RedisJwt({
+    //host: '/tmp/redis.sock', //unix domain
+    host: _Config.redis.host, //can be IP or hostname
+    port: _Config.redis.port, // port
+    maxretries: _Config.redis.maxretries, //reconnect retries, default 10
+    //auth: '123', //optional password, if needed
+    db: _Config.jwt.redisDb, //optional db selection
+    secret: _Config.jwt.secret, // secret key for Tokens!
+    multiple: _Config.jwt.multiple, // single or multiple sessions by user
+    kea: _Config.redis.kea, // Enable notify-keyspace-events KEA
+  }),
+};
 
 const __redisJwtSign = async payload => {
   return await new Promise((resolve, reject) => {
-    redisJwt
+    __instances.jwt
       .sign(payload.id, {
         ttl: _Config.jwt.ttl,
         dataToken: {
           email: payload.email,
         },
-        dataSession: { id: payload.id, email: payload.email },
+        dataSession: { email: payload.email },
       })
       .then(token => {
         resolve(token);
@@ -35,5 +37,6 @@ const __redisJwtSign = async payload => {
 module.exports = () => {
   return {
     redisJwtSign: __redisJwtSign,
+    instances: __instances,
   };
 };
